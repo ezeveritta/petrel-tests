b26be101e97b961917cabd3409de5d157bc8daea
Ezequiel E. Vera <37745672+ezeveritta@users.noreply.github.com>
2021-06-08 11:34:58 -0300

    antes de romper todo.. Tutorial completo y no probado..
    voy a borrar el vendor e instalar composer.

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Models\Usuario;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,4 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     ddd(Usuario::all()[0]->roles[0]->permisos->pluck('descripcion'));
     return view('welcome');
+});
+
+Route::get('test', function() {
+    //Storage::disk('google')->put('test.txt', 'Hello World');
 });
